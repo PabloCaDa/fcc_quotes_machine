0eb5e88c521d6f393437fa18a31f58919d42d981
PabloCaDa <pablo.cabreradarias@gmail.com>
2019-05-01 15:02:54 +0100

    Updates

---
+++ precache-manifest.45c416d9d9c630ac747b63e59e2bfe11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "402911c7b1f2e1846cad",
+    "url": "/fcc_quotes_machine/static/js/runtime~main.5bef19d6.js"
+  },
+  {
+    "revision": "4ea5bd54c3b47912f2bd",
+    "url": "/fcc_quotes_machine/static/js/main.7a846e66.chunk.js"
+  },
+  {
+    "revision": "6b666a3966779dc9b4d9",
+    "url": "/fcc_quotes_machine/static/js/2.b7fb4c35.chunk.js"
+  },
+  {
+    "revision": "4ea5bd54c3b47912f2bd",
+    "url": "/fcc_quotes_machine/static/css/main.0171b308.chunk.css"
+  },
+  {
+    "revision": "6b666a3966779dc9b4d9",
+    "url": "/fcc_quotes_machine/static/css/2.59fb6570.chunk.css"
+  },
+  {
+    "revision": "5490ce626883acec6f1bbb1e88c8c1b7",
+    "url": "/fcc_quotes_machine/index.html"
+  }
+];
