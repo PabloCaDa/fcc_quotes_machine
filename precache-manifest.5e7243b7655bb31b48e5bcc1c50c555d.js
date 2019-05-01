eef8ed548bf69a7f580983e9364919d16404e194
PabloCaDa <pablo.cabreradarias@gmail.com>
2019-05-01 16:15:39 +0100

    Updates

--- precache-manifest.5e7243b7655bb31b48e5bcc1c50c555d.js
+++ precache-manifest.5e7243b7655bb31b48e5bcc1c50c555d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/fcc_quotes_machine/static/js/runtime~main.5bef19d6.js"
   },
   {
-    "revision": "3b151ad0f5028e6b1a81",
-    "url": "/fcc_quotes_machine/static/js/main.730ced11.chunk.js"
+    "revision": "8b52c325b26425940dcc",
+    "url": "/fcc_quotes_machine/static/js/main.4457061f.chunk.js"
   },
   {
     "revision": "6b666a3966779dc9b4d9",
     "url": "/fcc_quotes_machine/static/js/2.b7fb4c35.chunk.js"
   },
   {
-    "revision": "3b151ad0f5028e6b1a81",
+    "revision": "8b52c325b26425940dcc",
     "url": "/fcc_quotes_machine/static/css/main.f42378af.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/fcc_quotes_machine/static/css/2.59fb6570.chunk.css"
   },
   {
-    "revision": "b698170e8bf0111f705dc1333d715e11",
+    "revision": "6f3d8bf663f47e7041a1c8f61d897b3d",
     "url": "/fcc_quotes_machine/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fcc_quotes_machine/precache-manifest.6114e0ce6aff7bcd508cdff96d068c42.js"
+  "/fcc_quotes_machine/precache-manifest.5e7243b7655bb31b48e5bcc1c50c555d.js"
 );
 
 workbox.clientsClaim();
